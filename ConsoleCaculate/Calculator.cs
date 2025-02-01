575c9bea5356606edb7599d8db2772c34602b666
unknown <@ngknn.local>
2025-02-01 10:56:44 +0300

    create calculator

---
+++ ConsoleCaculate/Calculator.cs
@@ -0,0 +1,105 @@
+﻿namespace ConsoleCaculate
+{
+    internal class Calculator
+    {
+        public double GetSumm(double valueOne, double valueTwo)
+        {
+            return valueOne + valueTwo;
+        }
+
+        public double GetMultiply(double valueOne, double valueTwo)
+        {
+            return valueOne * valueTwo;
+        }
+
+        public double GetDifference(double valueOne, double valueTwo)
+        {
+            return valueOne - valueTwo;
+        }
+
+        public double GetDivide(double valueOne, double valueTwo)
+        {
+            if (valueTwo == 0.0000000000)
+            {
+                throw new DivideByZeroException("Деление на 0 не возможно!");
+            }
+
+            return valueOne / valueTwo;
+        }
+
+        public double GetDegree(double value, double degree)
+        {
+            return Math.Pow(value, degree);
+        }
+
+        public double GetRoot(double value, double root)
+        {
+            //проверить корень
+            if (root == 0.0000000000)
+            {
+                throw new Exception("Не возмжно найти 0-вой корень!");
+            }
+
+            if (value < 0)
+            {
+                throw new Exception("Не возмжно найти корень отрицательного числа!");
+            }
+
+            return Math.Exp(1 / root * Math.Log(value));
+        }
+
+        public double GetProcent(double valueOne, double valueTwo)
+        {
+            if (valueOne < 0 && valueTwo >= 0 || valueTwo < 0 && valueOne >= 0)
+            {
+                throw new Exception("Проценты от значений с разными знакоми определить невозможно!");
+            }
+
+            return (valueOne * valueTwo) / 100;
+        }
+
+        public double GetLog(double value, double? footing)
+        {
+            if (footing == null || footing == 0)
+            {
+                return Math.Log(value);
+            }
+
+            if (footing < 0)
+            {
+                throw new Exception("Логарифм не может иметь отрицательно основание!");
+            }
+
+            if (footing == 1)
+            {
+                throw new Exception("Основание не должно быть равно единице!");
+            }
+
+            if (value <= 0)
+            {
+                throw new Exception("Итоговое число логарифма должно быть больше нуля!");
+            }
+
+            return Math.Log(value, footing.Value);
+        }
+
+        public double GetSin(double value)
+        {
+            return Math.Sin(value);
+        }
+
+        public double GetCtan(double value)
+        {
+            double sin = Math.Sin(value);
+
+            if (sin == 0.0000000000000)
+            {
+                throw new Exception("Значение для определения катангенса не может быть равно 0");
+            }
+
+            double ctan = Math.Cos(value) / Math.Sin(value);
+
+            return ctan;
+        }
+    }
+}
--- ConsoleCaculate/Program.cs
+++ ConsoleCaculate/Program.cs
@@ -2,9 +2,203 @@
 {
     internal class Program
     {
+        private static double ValueOne { get; set; }
+
+        private static double? ValueTwo { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Калькулятор\n");
+
+            Calculator calc = new Calculator();
+
+            double? result = null;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Выберите операцию:\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление\n5. Возведеие в степень\n6. Нахождение корня\n7. Процент от числа\n8. Логарифм числа\n9. Синус числа\n10. Катангенс числа\n0. Выход из программы\n\nВаш выбор: ");
+
+                    int operation = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Clear();
+
+                    switch (operation)
+                    {
+                        case 0:
+                            Console.WriteLine("До свидания!");
+                            return;
+                        case 1:
+                            Console.WriteLine("Сложение");
+                            GetData(2);
+                            result = calc.GetSumm(ValueOne, ValueTwo!.Value);
+                            break;
+                        case 2:
+                            Console.WriteLine("Вычитание");
+
+                            Console.WriteLine("Примечание: перыое число - уменьшаемое; второе число - вычитаемое\n");
+
+                            GetData(2);
+                            result = calc.GetDifference(ValueOne, ValueTwo!.Value);
+                            break;
+                        case 3:
+                            Console.WriteLine("Умножение");
+                            GetData(2);
+                            result = calc.GetMultiply(ValueOne, ValueTwo!.Value);
+                            break;
+                        case 4:
+                            Console.WriteLine("Деление");
+
+                            Console.WriteLine("Примечание: перыое число - делимое; второе число - делитель\n");
+
+                            GetData(2);
+                            try
+                            {
+                                result = calc.GetDivide(ValueOne, ValueTwo!.Value);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+                            break;
+                        case 5:
+                            Console.WriteLine("Возведеие в степень");
+
+                            Console.WriteLine("Примечание: перыое число - число, которое возводится в степень; второе число - степень\n");
+
+                            GetData(2);
+                            result = calc.GetDegree(ValueOne, ValueTwo!.Value);
+                            break;
+                        case 6:
+                            Console.WriteLine("Нахождение корня");
+
+                            Console.WriteLine("Примечание: перыое число - число, корень которого находится; второе число - вариация корня\n");
+
+                            GetData(2);
+                            try
+                            {
+                                result = calc.GetRoot(ValueOne, ValueTwo!.Value);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+                            break;
+                        case 7:
+                            Console.WriteLine("Процент от числа");
+
+                            Console.WriteLine("Примечание: в данном методе находится процентное отношение первого числа ко второму\n");
+
+                            GetData(2);
+
+                            try
+                            {
+                                result = calc.GetProcent(ValueOne, ValueTwo!.Value);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+                            break;
+                        case 8:
+                            Console.WriteLine("Логарифм числа");
+
+                            Console.WriteLine("Примечание: для нахождения натурального логарифма второе число (основание) можно не вводить\n");
+
+                            GetData(2);
+
+                            try
+                            {
+                                result = calc.GetLog(ValueOne, ValueTwo);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+                            break;
+                        case 9:
+                            Console.WriteLine("Синус числа");
+                            GetData(1);
+
+                            try
+                            {
+                                result = calc.GetSin(ValueOne);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+
+                            break;
+                        case 10:
+                            Console.WriteLine("Катангенс числа");
+                            GetData(1);
+
+                            try
+                            {
+                                result = calc.GetCtan(ValueOne);
+                            }
+                            catch (Exception ex)
+                            {
+                                PrintError(ex.Message);
+                                result = null;
+                            }
+
+                            break;
+                        default:
+                            throw new Exception("выбранного варината не предусмотрено!");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Не корректные данные!");
+                    Console.WriteLine("Повторите ввод!\n");
+                }
+
+                if (result != null) Console.WriteLine($"Результат вычислений: {result}\n");
+                
+                result = null;
+            }
+
+        }
+
+        private static void GetData(int countValues)
+        {
+            try
+            {
+                if (countValues == 1)
+                {
+                    Console.Write("Введите число для операции: ");
+                    ValueOne = Convert.ToDouble(Console.ReadLine());
+                }
+                else if (countValues == 2)
+                {
+
+                    Console.Write("Введите первое число для операции: ");
+                    ValueOne = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число для операции: ");
+                    ValueTwo = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введённые данные не корректны!");
+                Console.WriteLine("Повторите ввод...\n\n");
+
+                GetData(countValues);
+            }
+        }
+
+        private static void PrintError(string errorMessage)
+        {
+            Console.WriteLine($"Error: {errorMessage}\n");
         }
     }
 }
